77b15f43f387ceefc3b07016be985c09d7c595fb
Sugatraj <sugatraj.2106@gmail.com>
2025-02-17 16:10:48 +0530

    Revert "Switch to GitHub Pages deployment"
    This reverts commit aad804198934a18a7ce4cc21215b1ad23b4564b5.

--- vite.config.js
+++ vite.config.js
@@ -10,24 +10,4 @@ export default defineConfig({
     },
   },
   base: '/rokomed_pharma/',
-  build: {
-    outDir: 'dist',
-    sourcemap: true,
-    emptyOutDir: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-        },
-      },
-    },
-  },
-  server: {
-    port: 3000,
-    strictPort: true,
-    hmr: {
-      host: 'localhost',
-      protocol: 'ws'
-    }
-  }
 }) 
